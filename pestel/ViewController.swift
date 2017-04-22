9e76be1b952eb7666655806951005830c48d4e01
blkshdw <aleksey.glebow@gmail.com>
2017-04-22 20:13:08 +0300

    Initial Commit

---
+++ pestel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pestel
+//
+//  Created by Алексей on 22.04.17.
+//  Copyright © 2017 tetofa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
